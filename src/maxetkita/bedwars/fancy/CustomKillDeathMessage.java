90d07128d9fd6a6a7b33ffa3c9879affd4a2cd61
SuperMaxime38 <max.cardot.clement@icloud.com>
2023-03-13 19:01:38 +0100

    this is a test

--- src/maxetkita/bedwars/fancy/CustomKillDeathMessage.java
+++ src/maxetkita/bedwars/fancy/CustomKillDeathMessage.java
@@ -1,5 +1,15 @@
 package maxetkita.bedwars.fancy;
 
+import java.util.Random;
+
 public class CustomKillDeathMessage {
 //Do something
+	
+	private static void randomMessageGenerator() {
+		Random rdm = new Random();
+		int i = rdm.nextInt(0);
+		switch(i) {
+		//Do something
+		}
+	}
 }
